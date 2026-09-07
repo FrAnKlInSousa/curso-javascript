3f45845bb76a9a86df858dc8834ed67791029e9b
franklin <frank.fkn.23@gmail.com>
2026-09-06 23:02:19 -0300

    nomeacao e convencoes de nomeacoes de variaveis

---
+++ aula-81/variaveis.js
@@ -0,0 +1,22 @@
+// nomes invalidos
+
+// var my-name = 'franklin';
+// var my#name = 'franklin';
+// var my name = 'franklin';
+// var var = 'franklin';
+// var 123 = 123;
+
+
+// variaveis válidas
+var myname;
+var myName;  // camel case
+var my123 = 123;
+var myvar;
+var my_name;  // snake case
+
+// os únicos caracteres especiais permitidos são _ e $
+var _myName;
+var $myName;
+
+
+
